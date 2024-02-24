3426264892e8452b8f1e7cd360a7dc8da0ad821b
patrick-204 <p.ohara921@gmail.com>
2024-02-24 14:46:58 -0500

    Created a function head which returns the first item in the array. The item is then passed to assert function made earlier. Didn't seem to have to add more error catching.

---
+++ head.js
@@ -0,0 +1,25 @@
+// My own assert function
+// If values match, print Assertion Passed: [actual] === [expected]
+// Otherwise, Assertion Failed: [actual] !== [expected]
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤦‍♂️🤦‍♂️🤦‍♂️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Create a function head which returns the first item in the array
+// The head function should not return the first element as an array. It should simply return the element itself.
+const head = function(arr) {
+  // console.log(typeof(arr[0]));
+  const element = arr[0];
+  return element;
+};
+
+
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([5]), 5);
+assertEqual(head([]), 5);
