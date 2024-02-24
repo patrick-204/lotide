a585b3e03cf504841266b66265408b8a83d5d19f
patrick-204 <p.ohara921@gmail.com>
2024-02-24 15:35:26 -0500

    Added assertions for a function tail which returns every element except the head (first element) of the array.

---
+++ tail.js
@@ -0,0 +1,27 @@
+// My own assert function
+// If values match, print Assertion Passed: [actual] === [expected]
+// Otherwise, Assertion Failed: [actual] !== [expected]
+const assertEqual = function(actual, expected) {
+  // ensure get back 2 elements
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤦‍♂️🤦‍♂️🤦‍♂️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// Create a function tail which returns every element except the head (first element) of the array.
+const tail = function(arr) {
+  const elements = arr.slice(1);
+  // console.log(elements);
+  return elements;
+};
+
+const words = tail(["🪀🪀", "Lighthouse", "Labs"]);
+
+// TEST CODE
+assertEqual(words.length, 2);
+assertEqual(words[0], "Lighthouse");
+assertEqual(words[1], "Labs");
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
