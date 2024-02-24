f0e11ed84c68f6633519c64f465ddfde11a7e8be
patrick-204 <p.ohara921@gmail.com>
2024-02-24 13:59:00 -0500

    Made a custome assert function for testing if 2 args are equal.

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// My own assert function
+// If values match, print Assertion Passed: [actual] === [expected]
+// Otherwise, Assertion Failed: [actual] !== [expected]
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤦‍♂️🤦‍♂️🤦‍♂️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
