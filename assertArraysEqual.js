5f1c71582a9ba73201fc7397b14c3b91a3f815ca
patrick-204 <p.ohara921@gmail.com>
2024-02-25 16:27:12 -0500

    corrected assertion function to call eqarrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,8 @@
 // My own assert function for arrays
-const assertArraysEqual = function(eqArrays) {
-  if (!eqArrays) {
+const assertArraysEqual = function(arr1, arr2) {
+  if (!eqArrays(arr1, arr2)) {
     console.log(`🤦‍♂️🤦‍♂️🤦‍♂️ Assertion Failed: Array 1 !== array 2`);
-  } else if (eqArrays) {
+  } else if (eqArrays(arr1, arr2)) {
     console.log(`👍👍👍 Assertion Passed: Array 1 === Array 2`);
   }
 };
@@ -21,6 +21,6 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]));
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, "3"]));
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]));
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
