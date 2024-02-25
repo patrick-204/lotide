0d268736676a4dd6e52003a26d7252ede105c473
patrick-204 <p.ohara921@gmail.com>
2024-02-25 16:22:14 -0500

    corrected assertion function to call eqarrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,26 @@
 // My own assert function for arrays
-const assertArraysEqual = function(arr1, arr2) {
+const assertArraysEqual = function(eqArrays) {
+  if (!eqArrays) {
+    console.log(`🤦‍♂️🤦‍♂️🤦‍♂️ Assertion Failed: Array 1 !== array 2`);
+  } else if (eqArrays) {
+    console.log(`👍👍👍 Assertion Passed: Array 1 === Array 2`);
+  }
+};
+
+// Implement a function eqArrays which takes in two arrays and returns
+// true or false, based on a perfect match.
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    console.log(`🤦‍♂️🤦‍♂️🤦‍♂️ Assertion Failed: Array length 1 ${arr1.length} !== Array length 2 ${arr2.length}`);
+    return false;
   }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log(`🤦‍♂️🤦‍♂️🤦‍♂️ Assertion Failed: Array 1 element ${arr1[i]} !== Array 2 element ${arr2[i]}`);
+      return false;
     }
   }
-  console.log(`👍👍👍 Assertion Passed: ${arr1} === ${arr2}`);
+  return true;
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, "3"]);
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]));
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, "3"]));
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]));
