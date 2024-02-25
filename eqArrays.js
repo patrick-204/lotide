c5488d85cd08c453b8ba479174a29b7922457ef8
patrick-204 <p.ohara921@gmail.com>
2024-02-25 15:40:10 -0500

    Added a check for if arrays are the same size

--- eqArrays.js
+++ eqArrays.js
@@ -12,10 +12,10 @@ const assertEqual = function(actual, expected) {
 // Implement a function eqArrays which takes in two arrays and returns
 // true or false, based on a perfect match.
 const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1.length !== arr2.length) {
-      return false;
-    }
     if (arr1[i] !== arr2[i]) {
       return false;
     }
