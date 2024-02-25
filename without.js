f05af438d4c79aadc9a8b5db865d5e962ec7cbfd
patrick-204 <p.ohara921@gmail.com>
2024-02-25 17:23:56 -0500

    fixed the without function to have this capability no matter order of array.

--- without.js
+++ without.js
@@ -39,6 +39,7 @@ const without = function(source, itemsToRemove) {
 
 without([1, 2, 3], [1]); // => [2, 3]
 without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+without([1, 2, 3], [3, 2, 1]); // =>
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
