1d235c93605bd604bd93d775d69a6dcd5903b465
patrick-204 <p.ohara921@gmail.com>
2024-02-29 22:22:30 -0500

    countLetters() should take in a sentence (as a string) and then return an object that contains the count of each of the lettersin that sentence.

---
+++ countLetters.js
@@ -0,0 +1,46 @@
+// My own assert function
+// If values match, print Assertion Passed: [actual] === [expected]
+// Otherwise, Assertion Failed: [actual] !== [expected]
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤦‍♂️🤦‍♂️🤦‍♂️ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// countLetters() should take in a sentence (as a string) and then
+// return an object that contains the count of each of the letters
+// in that sentence.
+const countLetters = function(string) {
+  let output = {};
+  
+  for (const letter of string) {
+    if (letter !== ' ') {
+      if (output[letter]) {
+        output[letter] += 1;
+      } else {
+        output[letter] = 1;
+      }
+    }
+  }
+  return output;
+};
+
+const sentence = "lighthouse in the house";
+
+console.log(countLetters(sentence));
+
+// Test code
+assertEqual(countLetters(sentence), {
+  l: 1,
+  i: 2,
+  g: 1,
+  h: 4,
+  t: 2,
+  o: 2,
+  u: 2,
+  s: 2,
+  e: 3,
+  n: 1,
+});
