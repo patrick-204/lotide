5d0ab1fa4d5436cccd2c7f46bd7a858ea3e1cad9
patrick-204 <p.ohara921@gmail.com>
2024-02-29 22:27:52 -0500

    made assert tests better

--- countLetters.js
+++ countLetters.js
@@ -29,18 +29,11 @@ const countLetters = function(string) {
 
 const sentence = "lighthouse in the house";
 
-console.log(countLetters(sentence));
+const result = countLetters(sentence);
 
 // Test code
-assertEqual(countLetters(sentence), {
-  l: 1,
-  i: 2,
-  g: 1,
-  h: 4,
-  t: 2,
-  o: 2,
-  u: 2,
-  s: 2,
-  e: 3,
-  n: 1,
-});
+assertEqual(result['t'], 1);
+assertEqual(result['h'], 4);
+assertEqual(result['t'], 2);
+assertEqual(result['e'], 3);
+assertEqual(result['n'], 1);
