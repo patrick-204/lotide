3a9f71bd5b96b9b2608b525a1cab1b9ca7f8fd20
patrick-204 <p.ohara921@gmail.com>
2024-03-02 20:22:44 -0500

    My own assert function for objects. takes in two objects and console.logs an appropriate message to the console.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -19,7 +19,7 @@ const assertObjectsEqual = function(obj1, obj2) {
   const inspect = require('util').inspect;
   if (!eqObjects(obj1, obj2)) {
     console.log(`🤦‍♂️🤦‍♂️🤦‍♂️ Assertion Failed: ${inspect(obj1)} !== ${inspect(obj2)}`);
-  } else if (eqObjects(obj1, obj2)) {
+  } else {
     console.log(`👍👍👍 Assertion Passed: ${inspect(obj1)} === ${inspect(obj2)}`);
   }
 };
