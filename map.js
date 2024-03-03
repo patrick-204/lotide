6a6310917d4928c095704aae64d113a0f4309de8
patrick-204 <p.ohara921@gmail.com>
2024-03-03 15:29:26 -0500

    created a javascript map function

--- map.js
+++ map.js
@@ -1,9 +1,9 @@
 // My own assert function for arrays
 const assertArraysEqual = function(arr1, arr2) {
   if (!eqArrays(arr1, arr2)) {
-    console.log(`🤦‍♂️🤦‍♂️🤦‍♂️ Assertion Failed: Array 1 !== array 2`);
+    console.log(`🤦‍♂️🤦‍♂️🤦‍♂️ Assertion Failed: ${arr1} !== ${arr2}`);
   } else if (eqArrays(arr1, arr2)) {
-    console.log(`👍👍👍 Assertion Passed: Array 1 === Array 2`);
+    console.log(`👍👍👍 Assertion Passed: ${arr1} === ${arr2}`);
   }
 };
 
