b8de80bf9058a2470544f55bbeaec28346e1de84
patrick-204 <p.ohara921@gmail.com>
2024-03-13 23:03:37 -0400

    Added some import/export functionality to test modules.

--- head.js
+++ head.js
@@ -1,13 +1,16 @@
+// Import asserEqual value
+const assertEqual = require('./assertEqual');
+
 // My own assert function
 // If values match, print Assertion Passed: [actual] === [expected]
 // Otherwise, Assertion Failed: [actual] !== [expected]
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤦‍♂️🤦‍♂️🤦‍♂️ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🤦‍♂️🤦‍♂️🤦‍♂️ Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 // Create a function head which returns the first item in the array
 // The head function should not return the first element as an array. It should simply return the element itself.
@@ -17,9 +20,4 @@ const head = function(arr) {
   return element;
 };
 
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
+module.exports = head;
--- tail.js
+++ tail.js
@@ -17,11 +17,11 @@ const tail = function(arr) {
   return elements;
 };
 
-const words = tail(["🪀🪀", "Lighthouse", "Labs"]);
-
 // TEST CODE
+const words = tail(["🪀🪀", "Lighthouse", "Labs"]);
 assertEqual(words.length, 2);
 assertEqual(words[0], "Lighthouse");
 assertEqual(words[1], "Labs");
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(words[1], "Labs");
